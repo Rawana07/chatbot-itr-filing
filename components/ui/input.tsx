c3b5643790462e9c4897ab992f595cd5f22b98ba
Rawan <rawanaw0r1d@protonmail.com>
2025-04-04 13:17:24 +0530

    Delete components/ui directory

--- components/ui/input.tsx
+++
@@ -1,9 +0,0 @@
-export function Input({ type = 'text', ...props }: any) {
-  return (
-    <input
-      type={type}
-      className="border rounded w-full px-3 py-2"
-      {...props}
-    />
-  );
-}
